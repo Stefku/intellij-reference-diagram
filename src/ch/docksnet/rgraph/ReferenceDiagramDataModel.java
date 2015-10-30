29ae0119de65f8cd9d44e6943e86ef07434bc288
stefku <stefan.zeller@docksnet.ch>
2015-10-30 16:12:24 +0100

    remove node fixes #22

--- src/ch/docksnet/rgraph/ReferenceDiagramDataModel.java
+++ src/ch/docksnet/rgraph/ReferenceDiagramDataModel.java
@@ -180,6 +180,7 @@ public class ReferenceDiagramDataModel extends DiagramDataModel<PsiElement> {
 
     @Override
     public void removeNode(DiagramNode<PsiElement> node) {
+        nodes.remove(node);
     }
 
     @Override
